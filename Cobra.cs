d44ac89a9dba0a2e6eb8f028f8cb4007e88a4647
EmanuellePetrola <emanuelle.petrola@edu.senai.br>
2026-09-17 09:40:49 -0300

    Polimorfismo

---
+++ Cobra.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Polimorfismo
+{
+   public  class Cobra : Animal
+        {
+            public override void EmitirSom()
+            {
+                Console.WriteLine("Sssssss...");
+            }
+        }
+    }
+
---
+++ Macaco.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Polimorfismo
+{
+    
+      public  class Macaco : Animal
+        {
+            public override void EmitirSom()
+            {
+                Console.WriteLine("Ugh ugh aah aah!");
+            }
+        }
+
+    }
+
