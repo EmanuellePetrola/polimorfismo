6fd6879d23e0b4f435fe3719c7a3d5dfbb85e952
EmanuellePetrola <emanuelle.petrola@edu.senai.br>
2026-09-17 09:14:48 -0300

    Polimorfismo

---
+++ Desenvolvedor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Polimorfismo
+
+    {
+        class Desenvolvedor : Funcionario
+        {
+            public override double CalcularBonusAnual()
+            {
+                return Salario * 0.12;
+            }
+        }
+    }
+
+
