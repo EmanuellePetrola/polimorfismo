553a00e43d166d5e61eada2eba2a3035a67af879
EmanuellePetrola <emanuelle.petrola@edu.senai.br>
2026-09-17 08:42:03 -0300

    Adicionar arquivos de projeto.

---
+++ Geometria.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Polimorfismo
+{
+    public class Geometria
+        { 
+            public double CalcularArea(double lado)
+            {
+                return lado * lado;
+            }
+
+            public double CalcularArea(double baseRetangulo, double altura)
+            {
+                return baseRetangulo * altura;
+            }
+
+            public double CalcularArea(double raio, bool isCirculo)
+            {
+                return Math.PI * raio * raio;
+            }
+        }
+ 
+    }
+
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using Polimorfismo;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Geometria geometria = new Geometria();
+
+        double areaQuadrado = geometria.CalcularArea(8);
+
+        double areaRetangulo = geometria.CalcularArea(42, 6);
+
+        double areaCirculo = geometria.CalcularArea(3, true);
+
+        Console.WriteLine($"Área do quadrado: {areaQuadrado:F2}");
+        Console.WriteLine($"Área do retângulo: {areaRetangulo:F2}");
+        Console.WriteLine($"Área do círculo: {areaCirculo:F2}");
+    }
+}
+
+
+
+
+
+
